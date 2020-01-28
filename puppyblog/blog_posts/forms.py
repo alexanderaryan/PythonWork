0a779c1a43a4c5ac27b647a5e1a6dc34ea327f5b
alexanderaryan <alexanderaryan.m@gmail.com>
2020-01-29 00:24:45 +0530

    Deleting duplicated folder

--- puppyblog/blog_posts/forms.py
+++
@@ -1,8 +0,0 @@
-from puppyblog import FlaskForm,StringField,SubmitField,TextAreaField,DataRequired
-
-class BlogPostForm(FlaskForm):
-
-
-    title = StringField("Title",validators=[DataRequired()])
-    text = TextAreaField("Your Post goes here",validators=[DataRequired()])
-    submit = SubmitField("Post")
