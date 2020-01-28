9cb585c4d1367f0d6d83ef49f5c9c41e5be50c7d
alexanderaryan <alexanderaryan.m@gmail.com>
2020-01-29 00:29:52 +0530

    removed the duplicated directory

--- blogforpuppy/PythonWork/Randomgenerators.py
+++
@@ -1,8 +0,0 @@
-import random
-def randomgen(low,high,num):
-    for value in range(num):
-        yield (random.randint(low,high))
-
-x=randomgen(1,10,12)
-
-print (list(x))
--- blogforpuppy/PythonWork/puppyapp.py
+++
@@ -1,6 +0,0 @@
-from puppyblog import app
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
